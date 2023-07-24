1ba23676271205e6356b29fe66b3e8dc2993f0c2
Atanu Majumder <atanumajumder2004@gmail.com>
2023-07-24 22:47:46 +0530

    components and jsx

--- 01tutorial/src/App.js
+++ 01tutorial/src/App.js
@@ -2,19 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const randomName = () => {
+    const names = ["Dave", "Atanu", "Bob", "Kevin"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React with Atanu
-        </a>
+        <p>Hello {randomName()}!</p>
       </header>
     </div>
   );
