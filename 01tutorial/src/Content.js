c5bc111f42a918341270e092811a78fff4d09e65
Atanu Majumder <atanumajumder2004@gmail.com>
2023-08-01 19:14:34 +0530

    React click events

--- 01tutorial/src/Content.js
+++ 01tutorial/src/Content.js
@@ -4,9 +4,31 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+
+  const nameClick = () => {
+    console.log("You clicked it");
+  };
+  const nameClick2 = (name) => {
+    console.log(`${name} was clicked `);
+  };
+  const nameClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {randomName()}</p>
+      <p onDoubleClick={nameClick}>
+        Hello {randomName()}!
+      </p>
+      <button onClick={nameClick}>
+        Click It
+      </button>
+      <button onClick={() => nameClick2("Atanu Majumder")}>
+        Click It
+      </button>
+      <button onClick={(e) => nameClick3(e)}>
+        Click It
+      </button>
     </main>
   );
 };
