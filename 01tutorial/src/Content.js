e3ee8f87b33d3412e44151f13f018c7ed39f3f19
Atanu Majumder <atanumajumder2004@gmail.com>
2023-08-01 19:32:58 +0530

    React useState Hook

--- 01tutorial/src/Content.js
+++ 01tutorial/src/Content.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
+
 const Content = () => {
+  const [name, setName] = useState('Rahul');
+  const [count, setCount] = useState(0);
+
   const randomName = () => {
     const names = ["Dave", "Atanu", "Bob", "Kevin"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   };
 
   const nameClick = () => {
-    console.log("You clicked it");
+    setCount(count + 1)
+    console.log(count);
   };
+
   const nameClick2 = (name) => {
     console.log(`${name} was clicked `);
   };
+
   const nameClick3 = (e) => {
     console.log(e.target.innerText);
   };
@@ -18,17 +26,21 @@ const Content = () => {
   return (
     <main>
       <p onDoubleClick={nameClick}>
-        Hello {randomName()}!
+        Hello {name}!
       </p>
+
+      <button onClick={randomName}>
+        Change name
+      </button>
+
       <button onClick={nameClick}>
         Click It
       </button>
-      <button onClick={() => nameClick2("Atanu Majumder")}>
-        Click It
-      </button>
+
       <button onClick={(e) => nameClick3(e)}>
         Click It
       </button>
+
     </main>
   );
 };
